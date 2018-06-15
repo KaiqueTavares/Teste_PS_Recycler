57ba10bb8faeb514f11c97c2996c76ad0f39530a
KaiqueTavares <kaique-reino@hotmail.com.br>
2018-06-15 11:01:40 -0300

    Adicao de template de lista

---
+++ app/src/main/java/com/tavares/kaique/teste_ps_recycler/adapter/HeroiAdapter.kt
@@ -0,0 +1,17 @@
+package com.tavares.kaique.teste_ps_recycler.adapter
+
+import android.content.Context
+import android.widget.Adapter
+import com.tavares.kaique.teste_ps_recycler.model.Heroi
+import java.security.AccessControlContext
+import java.util.*
+
+/**
+ * Created by kaiqu on 15/06/2018.
+ */
+
+//class HeroiAdapter(private val herois: List <Heroi>,
+                  // val context: Context,
+                  // val listener: (Heroi) -> Unit): Adapter <HeroiAdapter.ViewHolder>(){
+
+//}
---
+++ app/src/main/java/com/tavares/kaique/teste_ps_recycler/model/Heroi.kt
@@ -0,0 +1,10 @@
+package com.tavares.kaique.teste_ps_recycler.model
+
+/**
+ * Created by kaique on 15/06/2018.
+ */
+
+data class Heroi (
+        val titulo : String,
+        val resourceId: Int,
+        val descricao: String)
